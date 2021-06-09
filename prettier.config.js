063fea63d0c0ae51296b1f29f309e4588bf3138d
CarlosPavajeau <cantte098@gmail.com>
2021-06-08 21:29:44 -0500

    build(deps): install and configure eslint

---
+++ prettier.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  singleQuote: true, // Force using single quotes
+  trailingComma: 'all', // Force the use of a comma at the end of objects
+  arrowParens: 'avoid', // Force not using parentheses with single parameter functions.
+}
