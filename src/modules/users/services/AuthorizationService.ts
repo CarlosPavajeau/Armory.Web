b74c34516c5ab5e09381595c3708d4cfea64fb47
CarlosPavajeau <cantte098@gmail.com>
2021-06-10 20:18:39 -0500

    feat: dispatch user actions in 'authorizeUser'

--- src/modules/users/services/AuthorizationService.ts
+++ src/modules/users/services/AuthorizationService.ts
@@ -1,13 +1,31 @@
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
+import { AppDispatch } from '../../../common/store';
 import { AuthenticationRequest } from '../models/AuthenticationRequest';
+import { incorrectPassword, loginSuccess, userNotFound } from '../userSlice';
 
 const authorizeUser = async (
   data: AuthenticationRequest,
-): Promise<AxiosResponse<string>> => {
-  return axios.post<string>(
-    `${process.env.REACT_APP_API_URL}/Authentication`,
-    data,
-  );
+  dispatch: AppDispatch,
+): Promise<void> => {
+  try {
+    const response = await axios.post<string>(
+      `${process.env.REACT_APP_API_URL}/Authentication`,
+      data,
+    );
+    if (response) {
+      dispatch(loginSuccess(response.data));
+    }
+  } catch (error) {
+    if (error.response.data.errors.UserNotFound) {
+      const errorMessage = error.response.data.errors.UserNotFound.join(', ');
+      dispatch(userNotFound(errorMessage));
+    }
+    if (error.response.data.errors.IncorrectPassword) {
+      const errorMessage =
+        error.response.data.errors.IncorrectPassword.join(', ');
+      dispatch(incorrectPassword(errorMessage));
+    }
+  }
 };
 
 export { authorizeUser };
