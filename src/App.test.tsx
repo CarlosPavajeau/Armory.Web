af1e1a3599b5066799495f1e97c2d8f44f7e49ac
CarlosPavajeau <cantte098@gmail.com>
2021-06-13 18:05:54 -0500

    test: fix app tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
+import { BrowserRouter } from 'react-router-dom';
+import { store } from './common/store';
 import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(
     <Provider store={store}>
-      <App />
-    </Provider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
   );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  expect(getByText(/Digite su contraseña de acceso/i)).toBeDefined();
 });
