74abbdc466c68c3052d04590cd4757446166b177
Carlos Pavajeau <cantte098@gmail.com>
2021-08-16 11:01:42 -0500

    test: update test suite

--- src/components/__tests__/data/DisplayDataHeader.test.tsx
+++ src/components/__tests__/data/DisplayDataHeader.test.tsx
@@ -1,28 +1,31 @@
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
+import '@testing-library/jest-dom';
 import DisplayDataHeader from '../../data/DisplayDataHeader';
 
 describe('DisplayDataHeader tests', () => {
-  test('Render display data header', () => {
-    const placeholder = 'Test placeholder';
-    let refresh = false;
+  const placeholder = 'Test placeholder';
+  let refresh = false;
 
-    const handleRefresh = () => {
-      refresh = !refresh;
-    };
+  const handleRefresh = () => {
+    refresh = !refresh;
+  };
+  let wrapper = shallow(
+    <DisplayDataHeader
+      placeholder={placeholder}
+      handleRefresh={handleRefresh}
+    />,
+  );
 
-    const result = render(
+  beforeEach(() => {
+    wrapper = shallow(
       <DisplayDataHeader
         placeholder={placeholder}
         handleRefresh={handleRefresh}
       />,
     );
-    expect(result).toBeTruthy();
-    expect(result.getByPlaceholderText(placeholder)).toBeTruthy();
-    expect(result.getByTitle('Refrescar datos')).toBeTruthy();
+  });
 
-    const button = result.getByTitle('Refrescar datos') as HTMLButtonElement;
-    button.click();
-
-    expect(refresh).toEqual(true);
+  test('Should be render DisplayDataHeader', () => {
+    expect(wrapper).toMatchSnapshot();
   });
 });
