c50cc2d840acc57c0068ebc18f84b6fb841e90c0
Carlos Pavajeau <cantte098@gmail.com>
2021-08-18 19:44:44 -0500

    feat: add QrReader and QrReaderDialog components

---
+++ src/components/qr/QrReader.tsx
@@ -0,0 +1,16 @@
+import { ReactElement } from 'react';
+import ReactQrReader from 'react-qr-reader';
+
+export interface QrReaderProps {
+  handleScan: (data: string | null) => void;
+  handleError: (err: any) => void;
+}
+
+const QrReader = (props: QrReaderProps): ReactElement => {
+  const { handleScan, handleError } = props;
+  return (
+    <ReactQrReader delay={200} onScan={handleScan} onError={handleError} />
+  );
+};
+
+export default QrReader;
---
+++ src/components/qr/QrReaderDialog.tsx
@@ -0,0 +1,58 @@
+import { ReactElement, useState } from 'react';
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+} from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import QrReader from './QrReader';
+import Alert from '../feedback/Alert';
+
+export interface QrReaderDialogProps {
+  open: boolean;
+  onClose: (value: string | null) => void;
+}
+
+const QrReaderDialog = (props: QrReaderDialogProps): ReactElement => {
+  const { open, onClose } = props;
+  const [hasError, setHasError] = useState(false);
+
+  const handleClose = () => {
+    setHasError(false);
+    onClose(null);
+  };
+
+  const handleScan = (value: string | null) => {
+    if (value != null) {
+      setHasError(false);
+      onClose(value);
+    }
+  };
+
+  const handleError = (err: any) => {
+    setHasError(true);
+  };
+
+  return (
+    <Dialog open={open} onClose={handleClose} maxWidth="xs" fullWidth>
+      <DialogTitle>Escanear QR</DialogTitle>
+      <DialogContent>
+        <Grid container alignItems="center">
+          <Grid item xs>
+            <QrReader handleScan={handleScan} handleError={handleError} />
+          </Grid>
+        </Grid>
+        {hasError && <Alert severity="error">No se pudo leer la imagen.</Alert>}
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose} variant="outlined" color="secondary">
+          Cancelar
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default QrReaderDialog;
