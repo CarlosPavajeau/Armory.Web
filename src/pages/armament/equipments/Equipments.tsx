562296c5ae3d57113f13778ab20fc4758f5e3519
Carlos Pavajeau <cantte098@gmail.com>
2021-09-03 09:47:09 -0500

    refactor: new Equipments component

--- src/pages/armament/equipments/Equipments.tsx
+++ src/pages/armament/equipments/Equipments.tsx
@@ -1,18 +1,21 @@
-import Paper from '@material-ui/core/Paper';
+import { Card } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import Stack from '@material-ui/core/Stack';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import { WithStyles } from '@material-ui/styles';
-import withStyles from '@material-ui/styles/withStyles';
-import clsx from 'clsx';
+import Typography from '@material-ui/core/Typography';
+import AddIcon from '@material-ui/icons/Add';
 import { useAppDispatch, useAppSelector } from 'common/hooks';
-import { displayData } from 'common/styles';
-import DisplayDataHeader from 'components/data/DisplayDataHeader';
+import DataListHead, { HeadLabel } from 'components/data/DataListHead';
+import DataListToolbar from 'components/data/DataListToolbar';
 import ApiErrors from 'components/feedback/ApiErrors';
 import CircularLoader from 'components/loading/CircularLoader';
+import Page from 'components/Page';
+import Scrollbar from 'components/scrollbar/Scrollbar';
 import { getEquipments } from 'modules/armament/equipments/Service';
 import {
   apiError,
@@ -21,13 +24,17 @@ import {
   selectEquipments,
   selectUiStatus,
 } from 'modules/armament/equipments/Slice';
-import { ReactElement, useCallback, useEffect } from 'react';
-import { Helmet } from 'react-helmet';
+import {
+  ChangeEvent,
+  MouseEvent,
+  ReactElement,
+  useCallback,
+  useEffect,
+  useState,
+} from 'react';
+import { Link as RouterLink } from 'react-router-dom';
 
-export type EquipmentsProps = WithStyles<typeof displayData>;
-
-const Equipments = (props: EquipmentsProps): ReactElement => {
-  const { classes } = props;
+const Equipments = (): ReactElement => {
   const dispatch = useAppDispatch();
   const equipments = useAppSelector(selectEquipments);
   const uiStatus = useAppSelector(selectUiStatus);
@@ -48,66 +55,118 @@ const Equipments = (props: EquipmentsProps): ReactElement => {
     })();
   }, [fetchEquipments]);
 
-  const handleRefresh = async () => {
-    await fetchEquipments();
+  const [order, setOrder] = useState<'asc' | 'desc'>('asc');
+  const [orderBy, setOrderBy] = useState('name');
+  const [filterName, setFilterName] = useState('');
+
+  const handleRequestSort = (
+    event: MouseEvent<HTMLSpanElement>,
+    property: string,
+  ) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
   };
 
+  const handleFilterByName = (
+    event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>,
+  ) => {
+    setFilterName(event.target.value);
+  };
+
+  const HEAD: HeadLabel[] = [
+    { id: 'code', label: 'Código', alignRight: false },
+    { id: 'type', label: 'Tipo', alignRight: false },
+    { id: 'model', label: 'Modelo', alignRight: false },
+    { id: 'series', label: 'N° de seríe', alignRight: false },
+    {
+      id: 'quantityAvailable',
+      label: 'Cantidad dispponible',
+      alignRight: false,
+    },
+  ];
+
   return (
-    <>
-      <Helmet>
-        <title>Armería | Equipos especiales y accesorios</title>
-      </Helmet>
-      <Paper>
-        <DisplayDataHeader
-          placeholder="Buscar equipo especial o accesorio"
-          handleRefresh={handleRefresh}
-        />
-        <Paper
-          elevation={0}
-          className={clsx(
-            (uiStatus === 'loading' || uiStatus === 'apiError') &&
-              classes.withoutData,
-          )}
-        >
-          {uiStatus === 'loading' && (
-            <CircularLoader
-              size={150}
-              message="Cargando equipos especiales y accesorios"
-            />
-          )}
-          {uiStatus === 'loaded' && (
-            <TableContainer className={classes.container}>
-              <Table stickyHeader>
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Codigo</TableCell>
-                    <TableCell>Tipo</TableCell>
-                    <TableCell>Modelo</TableCell>
-                    <TableCell>Series</TableCell>
-                    <TableCell>Cantidad disponible</TableCell>
-                  </TableRow>
-                </TableHead>
+    <Page title="Armería | Equipo especial y accesorios">
+      <Container>
+        <Stack>
+          <Typography variant="h4" gutterBottom>
+            Equipo especial y accesorios
+          </Typography>
+          <Button
+            variant="contained"
+            component={RouterLink}
+            to="/dashboard/equipments/register"
+            startIcon={<AddIcon />}
+          >
+            Agregar equipo especial o accesorio
+          </Button>
+        </Stack>
+
+        <Card>
+          <DataListToolbar
+            filterName={filterName}
+            placeholder="Buscar equipo especial o accesorio"
+            onFilterName={handleFilterByName}
+          />
+
+          <Scrollbar>
+            <TableContainer sx={{ minWidth: 800 }}>
+              <Table>
+                <DataListHead
+                  order={order}
+                  orderBy={orderBy}
+                  headLabel={HEAD}
+                  onRequestSort={handleRequestSort}
+                />
                 <TableBody>
-                  {equipments.map(equipment => {
-                    return (
-                      <TableRow key={equipment.code}>
-                        <TableCell>{equipment.code}</TableCell>
-                        <TableCell>{equipment.type}</TableCell>
-                        <TableCell>{equipment.model}</TableCell>
-                        <TableCell>{equipment.series}</TableCell>
-                        <TableCell>{equipment.quantityAvailable}</TableCell>
-                      </TableRow>
-                    );
-                  })}
+                  {uiStatus === 'loading' && (
+                    <TableRow>
+                      <TableCell
+                        align="center"
+                        colSpan={HEAD.length}
+                        sx={{ py: 3 }}
+                      >
+                        <CircularLoader
+                          size={150}
+                          message="Cargando equipo especial y accesorios..."
+                        />
+                      </TableCell>
+                    </TableRow>
+                  )}
+                  {uiStatus === 'loaded' &&
+                    equipments.length > 0 &&
+                    equipments.map(equipment => {
+                      const { code, type, model, series, quantityAvailable } =
+                        equipment;
+                      return (
+                        <TableRow key={code} tabIndex={-1} hover>
+                          <TableCell>{code}</TableCell>
+                          <TableCell>{type}</TableCell>
+                          <TableCell>{model}</TableCell>
+                          <TableCell>{series}</TableCell>
+                          <TableCell>{quantityAvailable}</TableCell>
+                        </TableRow>
+                      );
+                    })}
+
+                  <TableRow>
+                    <TableCell
+                      align="center"
+                      colSpan={HEAD.length}
+                      sx={{ py: 3 }}
+                    >
+                      <ApiErrors />
+                    </TableCell>
+                  </TableRow>
                 </TableBody>
               </Table>
             </TableContainer>
-          )}
-          <ApiErrors />
-        </Paper>
-      </Paper>
-    </>
+          </Scrollbar>
+        </Card>
+      </Container>
+    </Page>
   );
 };
 
-export default withStyles(displayData)(Equipments);
+export default Equipments;
