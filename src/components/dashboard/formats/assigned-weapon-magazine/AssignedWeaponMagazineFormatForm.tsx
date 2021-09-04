4d181e96ae6acfdcf25d0ef8e8a915ab531f5f2b
Carlos Pavajeau <cantte098@gmail.com>
2021-09-04 18:03:24 -0500

    feat(formats): make new RegisterAssignedWeaponMagazineFormat component

---
+++ src/components/dashboard/formats/assigned-weapon-magazine/AssignedWeaponMagazineFormatForm.tsx
@@ -0,0 +1,241 @@
+import { FormHelperText } from '@material-ui/core';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import Stack from '@material-ui/core/Stack';
+import TextField from '@material-ui/core/TextField';
+import { LoadingButton } from '@material-ui/lab';
+import AdapterMoment from '@material-ui/lab/AdapterMoment';
+import DatePicker from '@material-ui/lab/DatePicker';
+import LocalizationProvider from '@material-ui/lab/LocalizationProvider';
+import ApiErrors from 'components/feedback/ApiErrors';
+import CircularLoader from 'components/loading/CircularLoader';
+import Consola from 'consola';
+import { Form, FormikProvider, useFormik } from 'formik';
+import { CreateAssignedWeaponMagazineFormatRequest } from 'modules/formats/assigned-weapon-magazine/Models';
+import { createAssignedWeaponMagazineFormat } from 'modules/formats/assigned-weapon-magazine/Service';
+import { Warehouse } from 'modules/formats/war-material-and-special-equipment-assignment/Models';
+import { useSquadrons } from 'modules/squadrons/hooks';
+import { useSquadsBySquadron } from 'modules/squads/hooks';
+import moment from 'moment';
+import React, { ReactElement } from 'react';
+import { useNavigate } from 'react-router-dom';
+import * as Yup from 'yup';
+
+const AssignedWeaponMagazineFormatForm = (): ReactElement => {
+  const [squadrons, squadronsUiStatus] = useSquadrons();
+
+  const RegisterAssignedWeaponMagazineFormatSchema = Yup.object().shape({
+    code: Yup.string().required('Este campo es requerido'),
+    validity: Yup.date().required('Este campo es requerido'),
+    squadronCode: Yup.string().required('Este campo es requerido'),
+    squadCode: Yup.string().required('Este campo es requerido'),
+    warehouse: Yup.number().required('Este campo es requerido'),
+    comments: Yup.string(),
+  });
+
+  const navigate = useNavigate();
+  const formik = useFormik<CreateAssignedWeaponMagazineFormatRequest>({
+    initialValues: {
+      code: '',
+      validity: moment(),
+      squadronCode: '',
+      squadCode: '',
+      warehouse: Warehouse.Air,
+      date: moment(),
+      comments: '',
+    },
+    validationSchema: RegisterAssignedWeaponMagazineFormatSchema,
+    onSubmit: async values => {
+      try {
+        const result = await createAssignedWeaponMagazineFormat(values);
+        navigate(
+          `/dashboard/formats/assigned-weapon-magazine-format/items?formatId=${result}`,
+        );
+      } catch (err) {
+        if (process.env.NODE_ENV === 'development') {
+          Consola.error(err);
+        }
+      }
+    },
+  });
+
+  const { errors, touched, handleSubmit, isSubmitting, getFieldProps, values } =
+    formik;
+
+  const [squads, squadsUiStatus] = useSquadsBySquadron(values.squadronCode);
+
+  return (
+    <FormikProvider value={formik}>
+      <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
+        <Stack spacing={3}>
+          <TextField
+            label="Código"
+            helperText={
+              errors.code && touched.code
+                ? errors.code
+                : 'Digite el código del formato'
+            }
+            error={!!(errors.code && touched.code)}
+            disabled={isSubmitting}
+            {...getFieldProps('code')}
+            fullWidth
+          />
+
+          <LocalizationProvider dateAdapter={AdapterMoment}>
+            <DatePicker
+              label="Vigencia"
+              value={values.validity}
+              onChange={value => {
+                if (value) {
+                  formik.setFieldValue('validity', value);
+                }
+              }}
+              renderInput={params => (
+                <TextField
+                  helperText={
+                    errors.validity && touched.validity
+                      ? errors.validity
+                      : 'Digite la vigencia del formato'
+                  }
+                  error={!!(errors.validity && touched.validity)}
+                  fullWidth
+                  {...params}
+                />
+              )}
+            />
+          </LocalizationProvider>
+
+          <Stack direction={{ xs: 'column', sm: 'row' }} spacing={2}>
+            <FormControl fullWidth>
+              <InputLabel id="squadronCode-label">Escuadrilla</InputLabel>
+              <Select
+                labelId="squadronCode-label"
+                label="Escuadrilla"
+                error={!!(errors.squadronCode && touched.squadronCode)}
+                disabled={isSubmitting}
+                defaultValue=""
+                {...getFieldProps('squadronCode')}
+                fullWidth
+              >
+                {squadronsUiStatus === 'loading' && (
+                  <MenuItem value="">
+                    <CircularLoader size={40} message="Cargando escuadrillas" />
+                  </MenuItem>
+                )}
+                {squadronsUiStatus === 'apiError' && (
+                  <MenuItem value="">No hay datos</MenuItem>
+                )}
+                {squadronsUiStatus === 'loaded' &&
+                  squadrons.length > 0 &&
+                  squadrons &&
+                  squadrons.map(s => {
+                    return (
+                      <MenuItem value={s.code} key={s.code}>
+                        {s.name}
+                      </MenuItem>
+                    );
+                  })}
+              </Select>
+              <FormHelperText
+                error={!!(errors.squadronCode && touched.squadronCode)}
+              >
+                {errors.squadronCode && touched.squadronCode
+                  ? errors.squadronCode
+                  : 'Seleccione una escuadrilla'}
+              </FormHelperText>
+            </FormControl>
+            <FormControl fullWidth>
+              <InputLabel id="squadCode-label">Escuadra</InputLabel>
+              <Select
+                labelId="squadCode-label"
+                label="Escuadra"
+                error={!!(errors.squadCode && touched.squadCode)}
+                disabled={isSubmitting}
+                defaultValue=""
+                {...getFieldProps('squadCode')}
+                fullWidth
+              >
+                {squadsUiStatus === 'loading' && (
+                  <MenuItem value="">
+                    <CircularLoader size={40} message="Cargando escuadras" />
+                  </MenuItem>
+                )}
+                {squadsUiStatus === 'apiError' && (
+                  <MenuItem value="">No hay datos</MenuItem>
+                )}
+                {squadsUiStatus === 'loaded' &&
+                  squads &&
+                  squads.map(s => {
+                    return (
+                      <MenuItem value={s.code} key={s.code}>
+                        {s.name}
+                      </MenuItem>
+                    );
+                  })}
+                {squads && squads.length === 0 && (
+                  <MenuItem value="">No hay datos</MenuItem>
+                )}
+              </Select>
+              <FormHelperText error={!!(errors.squadCode && touched.squadCode)}>
+                {errors.squadCode && touched.squadCode
+                  ? errors.squadCode
+                  : 'Seleccione una escuadra'}
+              </FormHelperText>
+            </FormControl>
+          </Stack>
+
+          <FormControl fullWidth>
+            <InputLabel id="warehouse-label">Almacén de armamento</InputLabel>
+            <Select
+              labelId="warehouse-label"
+              label="Armacén de armamento"
+              error={!!(errors.warehouse && touched.warehouse)}
+              disabled={isSubmitting}
+              defaultValue={Warehouse.Terrestrial}
+              {...getFieldProps('warehouse')}
+              fullWidth
+            >
+              <MenuItem value={Warehouse.Terrestrial}>Terrestre</MenuItem>
+              <MenuItem value={Warehouse.Air}>Aéreo</MenuItem>
+            </Select>
+            <FormHelperText error={!!(errors.warehouse && touched.warehouse)}>
+              {errors.warehouse && touched.warehouse
+                ? errors.warehouse
+                : 'Seleccione un almacén de armamento'}
+            </FormHelperText>
+          </FormControl>
+
+          <TextField
+            label="Comentarios"
+            helperText={
+              errors.comments && touched.comments
+                ? errors.comments
+                : 'Digite los comentarios del formato'
+            }
+            error={!!(errors.comments && touched.comments)}
+            disabled={isSubmitting}
+            rows={4}
+            {...getFieldProps('comments')}
+            multiline
+            fullWidth
+          />
+
+          <ApiErrors />
+
+          <LoadingButton
+            size="large"
+            type="submit"
+            variant="contained"
+            loading={isSubmitting}
+          >
+            Registrar formato
+          </LoadingButton>
+        </Stack>
+      </Form>
+    </FormikProvider>
+  );
+};
+
+export default AssignedWeaponMagazineFormatForm;
