9284f07e010c24b173f47e0344e14132ecbe5cfd
Carlos Pavajeau <cantte098@gmail.com>
2021-09-28 10:21:42 -0500

    test: fix all tests

--- src/components/routes/Fallback.tsx
+++ src/components/routes/Fallback.tsx
@@ -1,30 +1,16 @@
-import { Theme } from '@mui/material';
 import Backdrop from '@mui/material/Backdrop';
 import CircularProgress from '@mui/material/CircularProgress';
-import { WithStyles } from '@mui/styles';
-import createStyles from '@mui/styles/createStyles';
-import withStyles from '@mui/styles/withStyles';
 import { ReactElement } from 'react';
 
-const fallBackStyles = (theme: Theme) =>
-  createStyles({
-    backdrop: {
-      color: '#fff',
-      zIndex: theme.zIndex.drawer + 1,
-      backdropFilter: 'blur(7px)',
-    },
-  });
-
-export type FallbackProps = WithStyles<typeof fallBackStyles>;
-
-const Fallback = (props: FallbackProps): ReactElement => {
-  const { classes } = props;
-
+const Fallback = (): ReactElement => {
   return (
-    <Backdrop open className={classes.backdrop}>
+    <Backdrop
+      open
+      sx={{ color: '#fff', zIndex: theme => theme.zIndex.drawer + 1 }}
+    >
       <CircularProgress color="inherit" size={100} />
     </Backdrop>
   );
 };
 
-export default withStyles(fallBackStyles)(Fallback);
+export default Fallback;
