c9973267e9d458320ac634d6a439a6a6ed54cae5
Carlos Pavajeau <cantte098@gmail.com>
2021-09-29 21:02:19 -0500

    feat(troopers): change field display order

--- src/pages/troopers/Troopers.tsx
+++ src/pages/troopers/Troopers.tsx
@@ -59,8 +59,8 @@ const Troopers = (): ReactElement => {
   const HEAD: HeadLabel[] = [
     { id: 'id', label: 'Id', alignRight: false },
     { id: 'degreeName', label: 'Grado', alignRight: false },
-    { id: 'rankName', label: 'Cargo de operación', alignRight: false },
     { id: 'name', label: 'Nombre', alignRight: false },
+    { id: 'rankName', label: 'Cargo de operación', alignRight: false },
     { id: 'fireteamName', label: 'Escuadra', alignRight: false },
   ];
 
@@ -136,11 +136,11 @@ const Troopers = (): ReactElement => {
                           <TableRow key={id} tabIndex={-1} hover>
                             <TableCell>{id}</TableCell>
                             <TableCell>{degreeName}</TableCell>
-                            <TableCell>{rankName}</TableCell>
                             <TableCell>
                               {firstName} {secondName} {lastName}{' '}
                               {secondLastName}
                             </TableCell>
+                            <TableCell>{rankName}</TableCell>
                             <TableCell>{fireteamName}</TableCell>
                           </TableRow>
                         );
