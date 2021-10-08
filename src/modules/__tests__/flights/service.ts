f53187d37481916118b4a3f8cab5cf1c07ae8782
Carlos Pavajeau <cantte098@gmail.com>
2021-10-08 11:27:22 -0500

    test(flights): add FlightsService tests

---
+++ src/modules/__tests__/flights/service.ts
@@ -0,0 +1,49 @@
+import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect';
+
+import { AxiosResponse } from 'axios';
+import HttpClient from 'common/config/http';
+import { CreateFlightRequest, Flights } from 'modules/flights/models';
+import FlightsService from 'modules/flights/service';
+
+import { Squads } from '../../squads/models';
+
+jest.mock('common/config/http');
+
+describe('Flights service test', () => {
+  const mockHttpClient = HttpClient as jest.Mocked<typeof HttpClient>;
+
+  test('Should be get flights', async () => {
+    const data: Flights = [
+      {
+        code: 'EF1',
+        name: 'Escuadrilla',
+        ownerName: 'Juan',
+      },
+    ];
+    const response: AxiosResponse<Squads> = {
+      data,
+      status: 200,
+      statusText: 'Ok',
+      config: {},
+      headers: {},
+    };
+    mockHttpClient.get.mockImplementationOnce(() => Promise.resolve(response));
+
+    const flights = await FlightsService.fetchAll();
+    expect(flights.length).toEqual(1);
+  });
+
+  test('Should be save Flight', async () => {
+    const request: CreateFlightRequest = {
+      code: 'EF1',
+      name: 'Escuadrilla',
+      squadCode: '123',
+      personId: '321',
+    };
+    mockHttpClient.post.mockImplementationOnce(() => Promise.resolve());
+
+    await FlightsService.create(request);
+    expect(mockHttpClient.post).toHaveBeenCalledWith('/Flights', request);
+  });
+});
