e00a193f512db2a54cf1db3eef4ac354c6517097
Carlos Pavajeau <cantte098@gmail.com>
2021-10-13 10:12:58 -0500

    test(ammunition): make AmmunitionService tests

---
+++ src/modules/__tests__/armament/ammunition/service.ts
@@ -0,0 +1,63 @@
+import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect';
+
+import { AxiosResponse } from 'axios';
+import HttpClient from 'common/config/http';
+import {
+  Ammunition,
+  CreateAmmunitionRequest,
+} from 'modules/armament/ammunition/models';
+import AmmunitionService from 'modules/armament/ammunition/service';
+
+jest.mock('common/config/http');
+
+describe('Ammunition service test', () => {
+  const mockHttpClient = HttpClient as jest.Mocked<typeof HttpClient>;
+  const data: Ammunition[] = [
+    {
+      lot: 'L20',
+      type: 'Guerra',
+      mark: 'INDUMIL',
+      caliber: '9 mm',
+      quantityAvailable: 100,
+      flightCode: 'EFF',
+    },
+  ];
+  const response: AxiosResponse<Ammunition[]> = {
+    data,
+    status: 200,
+    statusText: 'Ok',
+    config: {},
+    headers: {},
+  };
+
+  test('Should be get ammunition', async () => {
+    mockHttpClient.get.mockImplementationOnce(() => Promise.resolve(response));
+
+    const ammunition = await AmmunitionService.fetchAll();
+    expect(ammunition.length).toEqual(1);
+  });
+
+  test('Should be get ammunition by Flight', async () => {
+    mockHttpClient.get.mockImplementationOnce(() => Promise.resolve(response));
+
+    const ammunition = await AmmunitionService.fetchAllByFlight('EFF');
+    expect(ammunition.length).toEqual(1);
+  });
+
+  test('Should be save ammunition', async () => {
+    const request: CreateAmmunitionRequest = {
+      lot: 'L20',
+      type: 'Guerra',
+      mark: 'INDUMIL',
+      caliber: '9 mm',
+      quantityAvailable: 100,
+      flightCode: 'EFF',
+    };
+
+    mockHttpClient.post.mockImplementationOnce(() => Promise.resolve());
+
+    await AmmunitionService.create(request);
+    expect(mockHttpClient.post).toHaveBeenCalledWith('/Ammunition', request);
+  });
+});
